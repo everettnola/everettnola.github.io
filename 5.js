0ea9599f28edf142cc4337be1eb9743b787d3701
everettnola <140862634+everettnola@users.noreply.github.com>
2023-12-18 19:02:52 +0800

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,1 +1,1 @@
-window.location.href='https://docs.qingque.cn/d/home/eZQCbDmvwNgEVA-QjrvwEHtZv?identityId=23NVTRD5lf2'
+window.location.href='https://docs.qingque.cn/d/home/eZQAX4WTVzQQpepkn1VsG0Ftn?identityId=24I6jEcehtM'
